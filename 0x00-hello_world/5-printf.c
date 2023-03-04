c14e60598fb1749d63d401b25dad6bf2405153c0
Nana-Owusu <owusuagyei8@gmail.com>
2023-03-04 00:54:31 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+prinf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
